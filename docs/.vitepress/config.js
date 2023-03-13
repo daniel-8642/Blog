bc8058cc29d6df5bebe85dca6b3bc0a51a13a38d
王海涛 <859926750@qq.com>
2023-03-13 21:07:36 +0800

    导入docker实验计划文章

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,7 +6,17 @@ export default {
             {
                 text: 'Guide',
                 items: [
-                    { text: 'Introduction', link: '/' },
+                    { text: '简介', link: '/' },
+                ]
+            },
+            {
+                text: 'docker',
+                items: [
+                    { text: '容器简介', link: '/docker实验计划/index' },
+                    { text: '实验一: 环境准备', link: '/docker实验计划/实验一' },
+                    { text: '实验二: 构建镜像并且上传', link: '/docker实验计划/实验二' },
+                    { text: '实验三: 打通容器的文件系统', link: '/docker实验计划/实验三' },
+                    { text: '实验四: 让容器轻松互联', link: '/docker实验计划/实验四' },
                 ]
             }
         ]
