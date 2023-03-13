245d34307fd148fc2ee0d5e4d2b3eb286bbcf8fd
王海涛 <859926750@qq.com>
2023-03-13 21:31:47 +0800

    配置变更

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -12,7 +12,7 @@ export default {
             {
                 text: 'docker',
                 items: [
-                    { text: '容器简介', link: '/docker实验计划/index' },
+                    { text: '容器简介', link: '/docker实验计划/' },
                     { text: '实验一: 环境准备', link: '/docker实验计划/实验一' },
                     { text: '实验二: 构建镜像并且上传', link: '/docker实验计划/实验二' },
                     { text: '实验三: 打通容器的文件系统', link: '/docker实验计划/实验三' },
